f20dd608c04e0524f7eeade9d8c82744c147f72c
KHAKhazeus <khazeus@outlook.com>
2019-01-28 15:20:21 +0800

    result

---
+++ Eigenface/test.m
@@ -0,0 +1,88 @@
+clear;
+clc;
+
+meanFacemat = load('meanFace.mat');
+meanFace = meanFacemat.meanFace;
+% figure, imshow(uint8(meanFace)), title('meanFace');
+compressedFeatureVecmat = load('compressedFeatureVec.mat');
+compressedFeatureVec = compressedFeatureVecmat.compressedFeatureVec;
+bigXmat = load('bigX.mat');
+meanVector = load('meanVector.mat');
+meanVector = meanVector.meanVector;
+bigX = bigXmat.bigX;
+[row, rep] = size(bigX);
+
+% figure,title('featureFace')
+% for i = 1:3
+%     for j = 1:3
+%         featureFaceVec = compressedFeatureVec((i - 1)*3 + j , :);
+%         featureFace = reshape(featureFaceVec, [100, 100]);
+%         subplot(3, 3, (i - 1)*3 + j);
+%         imshow(uint8(featureFace));
+%     end
+% end
+
+% testImage = imread('./Yale2/training/subject15_5.bmp');
+% testSize = size(testImage);
+% testVec = reshape(testImage, [1, testSize(1) * testSize(2)]);
+% testVec = double(testVec');
+% testVec = testVec - meanVector;
+% compressedX = compressedFeatureVec * bigX;
+% compressedTest = compressedFeatureVec * testVec;
+% minDistance = cos_distance(compressedX(:, 1), compressedTest);
+% pos = 1;
+% for j = 1: rep
+%     newDistance = cos_distance(compressedX(:, j), compressedTest);
+%     if newDistance < minDistance
+%         minDistance = newDistance;
+%         pos = j;
+%     end
+% end
+% guess = floor((pos - 1) /8);
+% fprintf("这人大概是对象%d", uint8(guess + 1));
+
+total = 0;
+correct = 0;
+
+for i = 1: 15
+    for j = 9: 11
+        if i >=10
+            path = strcat('./Yale2/test/subject', int2str(i),'_',...
+                int2str(j), '.bmp');
+        else
+            path = strcat('./Yale2/test/subject0',int2str(i),'_',...
+                int2str(j), '.bmp');
+        end
+        testImage = imread(path);
+        testSize = size(testImage);
+        testVec = reshape(testImage, [1, testSize(1) * testSize(2)]);
+        testVec = double(testVec');
+        testVec = testVec - meanVector;
+        compressedX = compressedFeatureVec * bigX;
+        compressedTest = compressedFeatureVec * testVec;
+        minDistance = cos_distance(compressedX(:, 1), compressedTest);
+        pos = 1;
+        for j = 1: rep
+            newDistance = cos_distance(compressedX(:, j), compressedTest);
+            if newDistance < minDistance
+                minDistance = newDistance;
+                pos = j;
+            end
+        end
+        guess = uint8(floor((pos - 1) /8) + 1);
+%         fprintf("这人大概是对象%d", uint8(guess + 1));
+        if(guess == i)
+            correct = correct + 1;
+        end
+        total = total + 1;
+    end
+end
+
+fprintf('准确率:%f', (correct/total) * 100);
+
+function s = cos_distance(x, y)
+
+all = vertcat(x', y');
+s = pdist(all,'cosine');
+
+end
---
+++ Eigenface/train.m
@@ -0,0 +1,58 @@
+clear;
+clc;
+% 1.读取图片，拉成等长向量，拼接为像素值*(15*8)的向量
+% 2.行中心化
+% 3.特征向量
+% 4.W^T每一行是一个特征向量
+% 5.W^T的特征值进行排列
+
+for people = 1:15
+    for faceNumber = 1:8
+        if(people < 10)
+            path = strcat('./Yale2/training/subject0', int2str(people), '_', ...
+                int2str(faceNumber) , '.bmp');
+        else
+            path = strcat('./Yale2/training/subject', int2str(people), '_', ...
+                int2str(faceNumber) , '.bmp');
+        end
+        face = imread(path);
+        [row, col] = size(face);
+        faceFlattened = reshape(face, [1,row * col]);
+        bigX(:,(people-1) * 8 + faceNumber) = faceFlattened';
+    end
+end
+
+bigX = double(bigX);
+
+meanVector = mean(bigX, 2);
+for i = 1: 15*8
+    bigX(:, i) = bigX(:, i) - meanVector;
+end
+
+meanFace = reshape(meanVector, [100,100]);
+figure,imshow(uint8(meanFace)),title('meanFace');
+
+siCovX = bigX' * bigX;
+[siFVec, siFMat] = eig(siCovX);
+FVec = bigX * siFVec;
+FVecRow = FVec';
+totalVecRow = 0;
+matSize = size(siFMat);
+for i = matSize(1): -1 : 1
+    if siFMat(i,i) < 1
+        break;
+    end
+    totalVecRow = totalVecRow + 1;
+    compressedFeatureVec(totalVecRow, :) = FVecRow(i, :);
+end
+
+compressedSize = size(compressedFeatureVec);
+for j = 1: 15
+    featureVec = uint8(compressedFeatureVec(j, :));
+    featureFace = reshape(featureVec, [100, 100]);
+    figure, imshow(featureFace);
+end
+save('compressedFeatureVec.mat', 'compressedFeatureVec');
+save('bigX.mat', 'bigX');
+save('meanVector.mat', 'meanVector');
+save('meanFace.mat','meanFace');
